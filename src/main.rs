47b79ac80660481d5d5619c5812f9b48d726c234
Ruben Nijveld <ruben@gewooniets.nl>
2015-07-13 08:51:40 +0200

    Do nothing with karma changes if they occur in highlights to the bot

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ extern crate rustc_serialize;
 extern crate chrono;
 
 use docopt::Docopt;
-use dazeus::{DaZeus, EventType, Connection};
+use dazeus::{DaZeus, DaZeusClient, EventType, Connection};
 use handler::*;
 
 mod karma;
@@ -38,8 +38,21 @@ fn main() {
     let connection = Connection::from_str(socket).unwrap();
     let mut dazeus = DaZeus::new(connection);
 
+    dazeus.handshake("dazeus-karma", "1", None);
+
     dazeus.subscribe(EventType::PrivMsg, |evt, dazeus| {
-        handle_karma_events(&evt, dazeus);
+        let highlight_char = dazeus.get_highlight_char().unwrap_or("}".to_string());
+        let nick = dazeus.nick(&evt[0]).unwrap_or("DaZeus".to_string());
+
+        let hl_with_char = format!("{}karma", highlight_char);
+        let hl_with_nick = format!("{}:", nick);
+        let hl_with_nick_alt = format!("{},", nick);
+
+        let msg = &evt[3];
+
+        if !msg.starts_with(&hl_with_char[..]) && !msg.starts_with(&hl_with_nick[..]) && !msg.starts_with(&hl_with_nick_alt[..]) {
+            handle_karma_events(&evt, dazeus);
+        }
     });
 
     dazeus.subscribe_command("karma", |evt, dazeus| {
